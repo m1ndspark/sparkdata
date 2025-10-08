2e6901593a70662e3f91aa3b1284505d8cc53da3
Neil Heird <neil@mindsparkconsulting.com>
2025-10-08 14:44:44 -0400

    Update global_cache.py

--- utils/global_cache.py
+++ utils/global_cache.py
@@ -1,1 +1,8 @@
+# --------------------------------------------------
+# utils/global_cache.py
+# --------------------------------------------------
+# Shared in-memory caches used by multiple routes
+
+uploaded_data_cache = {}
+google_auth_cache = {}
 
